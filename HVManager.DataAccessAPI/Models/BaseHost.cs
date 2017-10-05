df50d702f0174eec1c798c20f4a93c916208c973
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-05 16:08:46 -0300

    Added data annotations e Network property

--- HVManager.DataAccessAPI/Models/BaseHost.cs
+++ HVManager.DataAccessAPI/Models/BaseHost.cs
@@ -14,15 +14,21 @@ namespace HVManager.DataAccessAPI.Models
         public int HostID { get; set; }
 
         [JsonProperty(Order = 1,Required = Required.Always)]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Name must have between 3 and 15 characters")]
         public string Name { get; set; }
 
         [JsonProperty(Order = 2, Required = Required.Always)]
         public OS OS { get; set; }
 
         [JsonProperty(Order = 5, Required = Required.Always)]
+        [Range(1, 768, ErrorMessage = "Memory size must be between 1GB and 768GB")]
         public int Memory { get; set; }
 
         [JsonProperty(Order = 6, Required = Required.Always)]
         public ICollection<Disk> Disks { get; set; }
+
+        [JsonProperty(Order = 7, Required = Required.Always)]
+        public ICollection<Network> Networks { get; set; }
+
     }
 }
