075f76b6e15d7543b2b5daa6f018d866852ab686
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-05 16:23:00 -0300

    Added VlanModel

---
+++ HVManager.DataAccessAPI/Models/Network.cs
@@ -0,0 +1,12 @@
+﻿namespace HVManager.DataAccessAPI.Models
+{
+    public class Network
+    {
+        public int NetworkID { get; set; }
+
+        public Vlan Vlan { get; set; }
+
+        public string IP { get; set; }
+        public string MAC { get; set; }
+    }
+}
---
+++ HVManager.DataAccessAPI/Models/Vlan.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HVManager.DataAccessAPI.Models
+{
+    public class Vlan
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int VlanID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Subnet { get; set; }
+
+    }
+}
