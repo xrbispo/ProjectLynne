6e4f303e522432c6e00a61851e3ca27c76942086
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-10 10:57:28 -0300

    Added HardwareModel property

---
+++ HVManager.DataAccessAPI/Models/HardwareModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HVManager.DataAccessAPI.Models
+{
+    public class HardwareModel
+    {
+        public int HWModelID { get; set; }
+
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Hardware model must have between 3 and 30 characters")]
+        [Display(Name = "Model Name")]
+        public string Name { get; set; }
+    }
+}
--- HVManager.DataAccessAPI/Models/PhysicalHost.cs
+++ HVManager.DataAccessAPI/Models/PhysicalHost.cs
@@ -1,5 +1,6 @@
 ﻿
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace HVManager.DataAccessAPI.Models
 {
@@ -10,5 +11,13 @@ namespace HVManager.DataAccessAPI.Models
 
         [JsonProperty(Order = 4, Required = Required.Always)]
         public int CoresPerSocket { get; set; }
+
+        //[JsonProperty(Order = 11, Required = Required.Always)]
+        //public Rack Rack { get; set; }
+
+        [JsonProperty(Order = 12, Required = Required.Always)]
+        [StringLength(20, MinimumLength = 7, ErrorMessage = "Serial/ServiceTag must have between 7 and 20 characters")]
+        [Display(Name = "Serial/ServiceTag")]
+        public HardwareModel Model { get; set; }
     }
 }
