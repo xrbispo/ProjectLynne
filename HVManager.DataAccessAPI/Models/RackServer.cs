fba68458bb9bd02aaf64040ac2db84053378594d
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-10 15:38:37 -0300

    New derived type RACKSERVER

---
+++ HVManager.DataAccessAPI/Models/RackServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HVManager.DataAccessAPI.Models
+{
+    public class RackServer : PhysicalHost
+    {
+        public Rack Rack { get; set; }
+    }
+}
