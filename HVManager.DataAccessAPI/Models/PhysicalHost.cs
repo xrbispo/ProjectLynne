5b5bc5ec9af6421ad3366f113bded6815b24607b
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-10 12:15:01 -0300

    Added properties SERIAL and WWPN

--- HVManager.DataAccessAPI/Models/PhysicalHost.cs
+++ HVManager.DataAccessAPI/Models/PhysicalHost.cs
@@ -12,10 +12,18 @@ namespace HVManager.DataAccessAPI.Models
         [JsonProperty(Order = 4, Required = Required.Always)]
         public int CoresPerSocket { get; set; }
 
-        //[JsonProperty(Order = 11, Required = Required.Always)]
-        //public Rack Rack { get; set; }
-
         [JsonProperty(Order = 12, Required = Required.Always)]
         public HardwareModel HardwareModel { get; set; }
+
+        [JsonProperty(Order = 13, Required = Required.Always)]
+        [StringLength(20, MinimumLength = 7, ErrorMessage = "Serial/ServiceTag must have between 7 and 20 characters")]
+        [Display(Name = "Serial/ServiceTag")]
+        public string Serial { get; set; }
+
+        [JsonProperty(Order = 14, Required = Required.Always)]
+        public string WWPN1 { get; set; }
+
+        [JsonProperty(Order = 15, Required = Required.Always)]
+        public string WWPN2 { get; set; }   
     }
 }
