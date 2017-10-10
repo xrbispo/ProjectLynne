6567c146a7e106663d487933515e9f909b6af992
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-10 16:23:39 -0300

    RackModel

---
+++ HVManager.DataAccessAPI/Models/Rack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HVManager.DataAccessAPI.Models
+{
+    public class Rack
+    {
+        public int RackID { get; set; }
+        public string Number { get; set; }
+    }
+}
--- HVManager.DataAccessAPI/Models/RackServer.cs
+++ HVManager.DataAccessAPI/Models/RackServer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ namespace HVManager.DataAccessAPI.Models
 {
     public class RackServer : PhysicalHost
     {
+        [JsonProperty(Order = 11, Required = Required.Always)]
         public Rack Rack { get; set; }
     }
 }
