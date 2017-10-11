362f300339bf581298c750c666c7916c3a97aedd
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-11 15:52:14 -0300

    Added DataAnnotation to Disk Model

--- HVManager.DataAccessAPI/Models/Disk.cs
+++ HVManager.DataAccessAPI/Models/Disk.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace HVManager.DataAccessAPI.Models
 {
     public class Disk
     {
         public int DiskID { get; set; }
+
+        [Required]
+        [Display(Name = "Size(GB)")]
         public int Size { get; set; }
 
         public BaseHost Host { get; set; }
