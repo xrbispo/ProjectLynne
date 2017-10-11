e8dbcaf81abebd9e280b0b97877f7d9547f9ee6c
Robson Reis Bispo <xrbispo@gmail.com>
2017-10-11 15:18:26 -0300

    Added RegEx to Network Model

--- HVManager.DataAccessAPI/Models/Network.cs
+++ HVManager.DataAccessAPI/Models/Network.cs
@@ -10,9 +10,14 @@ namespace HVManager.DataAccessAPI.Models
         public Vlan Vlan { get; set; }
 
         [Required]
+        [StringLength(18, MinimumLength = 11)]
+        
+        [RegularExpression(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$", ErrorMessage = "IP Address must be in this format 192.168.0.10/24")]
         public string IP { get; set; }
 
         [Required]
+        [StringLength(17,MinimumLength = 17)]
+        [RegularExpression(@"^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$", ErrorMessage = "MAC Address must be in this format 12:23:34:45:56:67")]
         public string MAC { get; set; }
 
         public BaseHost Host { get; set; }
